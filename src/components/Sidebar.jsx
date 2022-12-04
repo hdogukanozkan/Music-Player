c6281185ca2807fd70547b6aa548195d7b556d98
Hanifi Dogukan Ozkan <hanifidogukanozkan@gmail.com>
2022-12-04 21:49:45 +0300

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { RiCloseLine } from "react-icons/ri";
+import { HiOutlineMenu } from "react-icons/hi";
+
+import { logo } from "../assets";
+import { links } from "../assets/constants"; //menü isimleri yolları ve icon isimleri
+
+const NavLinks = ({ setMobileMenuOpen }) => (
+  <div className="mt-10 animate-slideleft">
+    {links.map((item) => (
+      <NavLink
+        key={item.name}
+        onClick={() => setMobileMenuOpen(false)}
+        to={item.to}
+        className="flex flex-row justify-start item-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+      >
+        <item.icon className="w-6 h-6 mr-2" />
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+);
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+        <img
+          src={logo}
+          alt="logo"
+          className="w-full h-14 object-contain animate-slideleft"
+        />
+        <NavLinks />
+      </div>
+
+      <div className="md:hidden absolute block right-3 top-6">
+        {mobileMenuOpen ? (
+          <RiCloseLine
+            className="w-6 h-6 text-white mr-2 cursor-pointer"
+            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+          />
+        ) : (
+          <HiOutlineMenu
+            className="w-6 h-6 text-white mr-2 cursor-pointer"
+            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+          />
+        )}
+      </div>
+
+      <div
+        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
+          mobileMenuOpen ? "left-0" : "-left-full"
+        }`}
+      >
+        <img
+          src={logo}
+          alt="logo"
+          className="w-full h-14 object-contain animate-slideleft"
+        />
+        <NavLinks setMobileMenuOpen={setMobileMenuOpen} />
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "088ff9fa80msh08c02716e076468p1526e9jsn6aac9ffe8d2d"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
